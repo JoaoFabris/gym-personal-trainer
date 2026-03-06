4396ee60d719d92a7917a264e13e773c752e7783
fabris <jvfabris21@gmail.com>
2026-03-06 18:10:05 -0300

    feat: enable workout-related routes by importing and registering home, me, stats, and ai routes

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ import {
 import z from "zod";
 
 import { auth } from "./lib/auth.js";
-// import { aiRoutes } from "./routes/ai.js";
-// import { homeRoutes } from "./routes/home.js";
-// import { meRoutes } from "./routes/me.js";
-// import { statsRoutes } from "./routes/stats.js";
+import { aiRoutes } from "./routes/ai.js";
+import { homeRoutes } from "./routes/home.js";
+import { meRoutes } from "./routes/me.js";
+import { statsRoutes } from "./routes/stats.js";
 import { workoutPlanRoutes } from "./routes/workout-plan.js";
 
 const app = Fastify({
@@ -73,10 +73,10 @@ await app.register(fastifyApiReference, {
 // RESTful
 // Routes
 await app.register(workoutPlanRoutes, { prefix: "/workout-plans" });
-// await app.register(homeRoutes, { prefix: "/home" });
-// await app.register(meRoutes, { prefix: "/me" });
-// await app.register(statsRoutes, { prefix: "/stats" });
-// await app.register(aiRoutes, { prefix: "/ai" });
+await app.register(homeRoutes, { prefix: "/home" });
+await app.register(meRoutes, { prefix: "/me" });
+await app.register(statsRoutes, { prefix: "/stats" });
+await app.register(aiRoutes, { prefix: "/ai" });
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
