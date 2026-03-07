620a4828f54ca8d7168e304134417cd242e42488
fabris <jvfabris21@gmail.com>
2026-03-07 17:18:10 -0300

    feat: update auth configuration to include social providers and adjust trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,12 +5,19 @@ import { openAPI } from "better-auth/plugins";
 import { prisma } from "./db.js";
 
 export const auth = betterAuth({
+  baseURL : process.env.BETTER_AUTH_URL,
   trustedOrigins: [
     "http://localhost:3000",// ← permite o Scalar fazer requests
-    "http://localhost:8081",    // 👈
-    "http://127.0.0.1:8081",   // 👈
-  ], //aqui ele permite q frontend fazer request para a api
+    "http://localhost:8081",    //
+    "http://127.0.0.1:8081",    //aqui ele permite q frontend fazer request para a api
 
+  ],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
